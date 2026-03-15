4ad1bdc05b5f83b0443e047bb6a6a9f77d52ea27
Trulle1234 <leonard.trulsson@gmail.com>
2026-03-15 21:11:17 +0100

    did some other stuff and changed out the intro to not be svelte to prevent weird behavior

--- photography-ysws/src/routes/+page.svelte
+++ photography-ysws/src/routes/+page.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
 	import '../app.css';
-    import { onMount } from 'svelte';
-    import { blur } from 'svelte/transition';
+	import { onMount } from 'svelte';
 
 	let show = $state(false);
 
 	onMount(() => {
 		show = true;
-        console.log("loaded");
 	});
 </script>
 
@@ -19,38 +17,36 @@
 	/>
 </a>
 
+<div id="main" class:visible={show}>
+	<div id="hero">
+		<h1 id="title">Darkroom</h1>
 
-{#if show}
-    <div id="main" transition:blur={{ duration: 1000 }}>
-        <div id="hero">
-            <h1 id="title">Darkroom</h1>
-
-            <p id="description">
-                Build something photography related<br>
-                Get cameras, gear, film and other cool stuff!
-            </p>
-        </div>
-    </div>
-{/if}
-
+		<p id="description">
+			Build something photography related<br>
+			Get cameras, gear, film and other cool stuff!
+		</p>
+	</div>
+</div>
 
 <img 
-    src="https://cdn.hackclub.com/019cf2b0-6906-7246-8451-490d4e152f0d/wave.svg"
-    alt="Wave"
-    id="wave"
+	src="https://cdn.hackclub.com/019cf2b0-6906-7246-8451-490d4e152f0d/wave.svg"
+	alt="Wave"
+	id="wave"
 />
 
 <div id="dark">
-    <h6 id="get">Get stuff like:</h6>
-    
-    <div id="polaroid-grid">
-        {#each { length: 6 }}
-            <img 
-                src="https://cdn.hackclub.com/019cf277-b514-7a8f-96f5-c33a5dc49504/polaroid.svg"
-                alt="Polaroid"
-                class="polaroid"
-                id="polaroid-1"
-            />
-        {/each}
-    </div>
+	<div class:visible={show}>
+		<h6 id="get">Get stuff like:</h6>
+		<i id="det-disclaimer">*This is subject to change!</i>
+		
+		<div id="polaroid-grid">
+			{#each Array(6) as _, i}
+				<img 
+					src="https://cdn.hackclub.com/019cf277-b514-7a8f-96f5-c33a5dc49504/polaroid.svg"
+					alt="Polaroid"
+					class="polaroid"
+				/>
+			{/each}
+		</div>
+	</div>
 </div>
